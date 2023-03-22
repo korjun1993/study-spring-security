f4c52822b1322a77f025c271e4aca51bb38a7b5f
김호준 <corojoon93@naver.com>
2023-03-23 01:25:39 +0900

    feat: Intellij.http 테스트 추가 (postman 대체)

--- src/main/java/com/example/security/AppApplication.java
+++ src/main/java/com/example/security/AppApplication.java
@@ -1,7 +1,13 @@
 package com.example.security;
 
+import com.example.security.domain.Account;
+import com.example.security.domain.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class AppApplication {
@@ -10,4 +16,18 @@ public class AppApplication {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner bootstrapTestAccount(AccountRepository accountRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			Account account = new Account();
+			account.setPassword(passwordEncoder.encode("1234"));
+
+			accountRepository.save(account);
+		};
+	}
 }
